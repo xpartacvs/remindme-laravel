400d7dbd915cec5433927c75ea138bd8f2bc1f14
Yudi Primaputra <xpartacvs@gmail.com>
2023-12-26 14:32:21 +0000

    feat(migration): add migration for reminders table
    add reminders table migration
    
    add Reminder model

---
+++ src/database/migrations/2023_12_26_092519_create_reminders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reminders', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->unsignedInteger('remind_at');
+            $table->unsignedInteger('event_at');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reminders');
+    }
+};
